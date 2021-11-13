4f73aca666c7098789e674e6cda7acfb9602b0c9
tobiasz.porebski <tobiasz.porebski@gmail.com>
2021-11-13 18:06:07 +0100

    Add store system tests

--- resources/store.py
+++ resources/store.py
@@ -6,7 +6,7 @@ class Store(Resource):
     def get(self, name):
         store = StoreModel.find_by_name(name)
         if store:
-            return store.json()
+            return store.json(), 200
         return {'message': 'Store not found'}, 404
 
     def post(self, name):
@@ -26,9 +26,9 @@ class Store(Resource):
         if store:
             store.delete_from_db()
 
-        return {'message': 'Store deleted'}
+        return {'message': 'Store deleted'}, 200
 
 
 class StoreList(Resource):
     def get(self):
-        return {'stores': [store.json() for store in StoreModel.query.all()]}
+        return {'stores': [store.json() for store in StoreModel.query.all()]}, 200
